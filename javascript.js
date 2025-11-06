71d3854e84ef33ed41597daf5027569f4e4d1303
ajinkya khairnar <ajinkyakhairnar08@gmail.com>
2025-11-07 00:54:22 +0530

    update

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 document.querySelector('.scrolldown').addEventListener('click', () => {
   document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
 });
+const header = document.querySelector('.header');
+const navbar = document.querySelector('.navbar');
+const navLinks = document.querySelector('.nav-links');
+
+
